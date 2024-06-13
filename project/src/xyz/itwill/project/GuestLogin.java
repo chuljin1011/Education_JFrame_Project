0ffe8eb7ede0f6794f61d9436eaed61acab4c653
itwill <itwill@DESKTOP-UPNOSB0>
2024-06-13 17:19:05 +0900

    2400613

--- project/src/xyz/itwill/project/GuestLogin.java
+++ project/src/xyz/itwill/project/GuestLogin.java
@@ -1,48 +1,33 @@
 package xyz.itwill.project;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.JTabbedPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.Vector;
 
 import javax.swing.JButton;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import xyz.itwill.project.dao.DesignerDAO;
+import xyz.itwill.project.dao.DesignerDTO;
 
 public class GuestLogin extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
-	private JTable table_1;
+	private JTable dTable;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GuestLogin frame = new GuestLogin();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
-	/**
-	 * Create the frame.
-	 */
 	public GuestLogin() {
 		setTitle("비회원 로그인");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -71,17 +56,17 @@ public class GuestLogin extends JFrame {
 		
 
 		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		tabbedPane.addTab("디자이너 검색", null, scrollPane_1, null);
+		JScrollPane dScrollPane = new JScrollPane();
+		tabbedPane.addTab("디자이너 검색", null, dScrollPane, null);
 		
-		table_1 = new JTable();
-		scrollPane_1.setViewportView(table_1);
-		table_1.setModel(new DefaultTableModel(new Object[][] {},
+		dTable = new JTable();
+		dScrollPane.setViewportView(dTable);
+		dTable.setModel(new DefaultTableModel(new Object[][] {},
                 new String[] {"이름","직급","경력"}));
-		table_1.setEnabled(false);
-		table_1.getTableHeader().setReorderingAllowed(false);
-		table_1.getTableHeader().setResizingAllowed(false);
-		
+		dTable.setEnabled(false);
+		dTable.getTableHeader().setReorderingAllowed(false);
+		dTable.getTableHeader().setResizingAllowed(false);
+		displayAllDsigner();
 				
 		
 		JPanel panel = new JPanel();
@@ -122,4 +107,27 @@ public class GuestLogin extends JFrame {
 		panel.add(btnNewButton, gbc_btnNewButton);
 				
 	}	
+	
+	public void displayAllDsigner() {
+		List<DesignerDTO> designerList = DesignerDAO.getDAO().selectDesignerAll();
+		
+		if(designerList.isEmpty()) {	//표 초기화 코드 제외 
+			return;
+		}
+		
+		DefaultTableModel defaultTableModel = (DefaultTableModel)dTable.getModel();
+		
+		for(DesignerDTO designer : designerList) {
+			Vector<Object> rowData = new Vector<Object>();
+			
+			rowData.add(designer.getName());
+			rowData.add(designer.getRank());
+			rowData.add(designer.getCareer());
+			
+			defaultTableModel.addRow(rowData);
+						
+		}
+			
+	}
+	
 }
--- project/src/xyz/itwill/project/dao/CustomerDAO.java
+++ project/src/xyz/itwill/project/dao/CustomerDAO.java
@@ -1,5 +1,5 @@
 package xyz.itwill.project.dao;
-
+ 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
